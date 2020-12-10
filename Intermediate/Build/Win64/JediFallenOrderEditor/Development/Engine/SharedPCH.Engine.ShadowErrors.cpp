a2214c6e2818fabcfe051e2fc74d9d2cb45dde0d
Ahowells96 <andrewhowells96@hotmail.com>
2020-12-10 20:51:37 +0000

    Changing Project to Blueprint

--- Intermediate/Build/Win64/JediFallenOrderEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Personal Work/UE4 Projects/JediFallenOrder/Intermediate/Build/Win64/JediFallenOrderEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Source/JediFallenOrder/JediFallenOrder.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "JediFallenOrder.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JediFallenOrder, "JediFallenOrder" );
- 
